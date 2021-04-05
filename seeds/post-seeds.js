72a75aae25fb4930dc984faaf05b0d67ac5d7fab
chris6661 <74381468+chris6661@users.noreply.github.com>
2021-04-05 00:03:52 -0500

    made serverjs vars more readable

--- seeds/post-seeds.js
+++ seeds/post-seeds.js
@@ -5,18 +5,18 @@ const {
 const postData = [{
         title: 'The chucker of wood',
         content: 'As of time of wriing, it is unknown how much wood a woodchuck would chuck if a woodchuck could chuck wood.',
-        user_id: 1
+        user_id: 2
 
     },
     {
         title: 'Motivational Phrases',
         content: 'The phrases to start your day with, everyday.',
-        user_id: 2
+        user_id: 3
     },
     {
         title: 'Most effective game preparation methods',
         content: 'Is practice the best way to prepare for a game?',
-        user_id: 3
+        user_id: 1
     }
 ];
 
